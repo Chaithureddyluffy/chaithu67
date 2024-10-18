f1c889db3dcf3bef31538d249ec0db2fd69a1334
chaithu <poolachaithanyareddy@gmail.com>
2024-10-18 15:47:04 +0530

    this is my commit

---
+++ src/main/scala/chaithuuuu.scala
@@ -0,0 +1,13 @@
+object chaithuuuu {
+  def main(args:Array[String]):Unit={
+    println("welcomeee")
+    println("commit2")
+    println("commit3")
+    println("commit4")
+    println("commit43")
+    println("commit 5")
+    println("commit7777")
+  }
+
+
+}
